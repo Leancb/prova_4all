2883ff83ffe19f5106c769d838444ab550b3f45f
Leancb <leancb@hotmail.com>
2019-09-23 01:14:55 -0300

    Commit inical

---
+++ src/test/java/Feature/automacaoProvaBDD.feature
@@ -0,0 +1,18 @@
+# new feature
+# Tags: optional
+# language: pt
+
+Funcionalidade:
+  Eu, enquanto candito a vaga de QA da 4All
+  Preciso automatizar o teste proposto para vaga
+  Para garantir aprovação no processo seletivo.
+  https://www.youtube.com/watch?v=5eezIEYqfCo&list=PL6tu16kXT9PpteusHGISu_lHcV6MbBtA6&index=3
+
+  Esquema do Cenário: :  Desafio 1
+    Dado     que configurei ambiente para acessar URL Chrome
+    Quando    abri tela do simulador de investimento
+    Então     Retorna a tela com resultado
+    Exemplos:
+      | valorPoupar |tmpAplic|
+      | 100,00      |3       |
+      | 200,00      |4       |
---
+++ src/test/java/Steps/prova.java
@@ -0,0 +1,54 @@
+package Steps;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class prova {
+
+    private WebDriver navegador;
+
+
+    @Dado("^que configurei ambiente para acessar URL Chrome$")
+    public void configuraDriverChrome() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\leanc\\Documents\\GitHub\\prova_4all\\src\\main\\resources\\chromedriver.exe");
+    }
+
+    @Dado("^que configurei ambiente para acessar URL Firefox$")
+    public void configuraDriverFirefox() {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\leanc\\Documents\\GitHub\\prova_4all\\src\\main\\resources\\geckodriver.exe");
+    }
+
+    @Quando("^abri tela do simulador de investimento$")
+    public void abreSimulador() {
+        navegador = new ChromeDriver();
+        navegador.get("https://shopcart-challenge.4all.com/");
+        navegador.manage().window().maximize();
+
+    }
+
+    @Entao("^Retorna a tela com resultado$")
+    public void retornaSimulacao() throws InterruptedException {
+
+        Thread.sleep(500);
+/*
+        String var2;
+
+        var2 = navegador.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div/div[2]/a")).getText();
+
+        if (var2 == null) {
+            System.out.println("\nErro, algo não funcionou");
+        } else {
+            Assert.assertTrue(var2.contains("REFAZER A SIMULAÇÃO"));
+        }
+*/
+        navegador.quit();
+    }
+
+
+}
